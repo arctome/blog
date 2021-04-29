5a5575922be89203a140694f1470299301d06fc5
arctome <sparkinglemon@foxmail.com>
2021-04-29 05:18:05 +0000

    test: fix first page (index) pagi caused posts lost

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export const pageQuery = graphql`
     }
     allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC},
-      limit: 5
+      limit: 10
     ) {
       nodes {
         excerpt
